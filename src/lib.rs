4c142515097fbb29c685e350c1af8b7a8fc4d907
myzero1 <myzero1@qq.com>
2021-05-23 20:04:39 +0800

    use std to get timestrap

--- src/lib.rs
+++ src/lib.rs
@@ -13,46 +13,38 @@
 //! ### run command
 //! 
 //! `z1info=` must be placed at the end of the command line,`cargo run`will write extra information to tmp file.
-//! ```
-//! cargo run p1 p2 z1info=version:1.2.3,compiler:myzero1
-//! ```
+//! - `cargo run p1 p2 z1info=version:1.2.3,compiler:myzero1`
 //! ### build
-//! ```
-//! cargo build
-//! cargo build --release
-//! ```
+//! - cargo build
+//! - OR cargo build --release
 //! 
 //! # Result
 //! ### run command or run `builded binary package`
+//! - cargo run  p1 p2
+//! - OR run builded binary package
 //! 
-//! ```
-//! cargo run  p1 p2
-//! or run builded binary package
-//! ```
+//! ### The Result
 //! 
-//! ```no_run
+//! <br/>=============== z1info extended data ====================
+//! <br/>| Extended data added to binary file through z1info.
+//! <br/>|--------------- z1info parameter ----------------------
+//! <br/>| z1info=version:1.2.3,compiler:myzero1
+//! <br/>|--------------- git info ------------------------------
+//! <br/>| commit id: 94896476ea1696f9b8764cd845f225e4af586bc4
+//! <br/>|--------------- build time ----------------------------
+//! <br/>| 1621770625
+//! <br/>=========================================================
 //! 
-//! =============== z1info extended data ====================
-//! | Extended data added to binary file through z1info.
-//! |--------------- z1info parameter ----------------------
-//! | z1info=version:1.2.3,compiler:myzero1
-//! |--------------- git info ------------------------------
-//! | commit id: 94896476ea1696f9b8764cd845f225e4af586bc4
-//! |--------------- build time ----------------------------
-//! | 2021-05-22 18:33:57
-//! =========================================================
-//! 
-//! ```
 
 use std::io::Write;
-use chrono::{Local};
 use std::process::{Command, Stdio};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 /// Add or display additional information
 ///
 /// # Examples
 ///
-/// ```
+/// ```no_run
 /// z1info_rust::run("z1template");
 /// // z1info_test::run("z1_info:{z1_info},git_info:{git_info},build_time:{build_time}");
 /// ```
@@ -79,7 +71,7 @@ pub fn run(template: &str){
 
         let mut content = str::replace(content_template,"{z1_info}",&args[args.len()-1]);
         content = str::replace(&content[..],"{git_info}",&get_commit_id());
-        content = str::replace(&content[..],"{build_time}",&format!("{}",Local::now().naive_local().format("%Y-%m-%d %H:%M:%S").to_string()));
+        content = str::replace(&content[..],"{build_time}",&format!("{}",get_current_timestamp()));
 
         let mut file = std::fs::File::create("z1info_tmp").unwrap();
         file.write_all(content.as_bytes()).expect("write z1info failed");
@@ -131,4 +123,13 @@ fn get_commit_id()->String{
     let ret_end = str::replace(&out_str[..],"\n","");
 
     return ret_end;
+}
+
+fn get_current_timestamp()->u64{
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+
+    return since_the_epoch.as_secs();
 }
