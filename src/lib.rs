bac18b240d2bec0672f84250a7b7a076cf011c8f
qinxuanwu <qinxuanwu@yungengxin.com>
2021-05-24 14:43:08 +0800

    add tmp info to tmp of registry

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 //! Add extra information to the binary package through `cargo run [...] z1info=extra_information`
-//! 
+//!
 //! # Use
-//! 
+//!
 //! ### add code
-//! 
+//!
 //! ```no_run
 //! fn main() {
 //!     z1info_rust::run("z1template");
@@ -11,20 +11,20 @@
 //! }
 //! ```
 //! ### run command
-//! 
+//!
 //! `z1info=` must be placed at the end of the command line,`cargo run`will write extra information to tmp file.
 //! - `cargo run p1 p2 z1info=version:1.2.3,compiler:myzero1`
 //! ### build
 //! - cargo build
 //! - OR cargo build --release
-//! 
+//!
 //! # Result
 //! ### run command or run `builded binary package`
 //! - cargo run  p1 p2
 //! - OR run builded binary package
-//! 
+//!
 //! ### The Result
-//! 
+//!
 //! <br/>=============== z1info extended data ====================
 //! <br/>| Extended data added to binary file through z1info.
 //! <br/>|--------------- z1info parameter ----------------------
@@ -34,7 +34,7 @@
 //! <br/>|--------------- build time ----------------------------
 //! <br/>| 1621770625
 //! <br/>=========================================================
-//! 
+//!
 
 use std::io::Write;
 use std::process::{Command, Stdio};
@@ -48,10 +48,10 @@ use std::time::{SystemTime, UNIX_EPOCH};
 /// z1info_rust::run("z1template");
 /// // z1info_test::run("z1_info:{z1_info},git_info:{git_info},build_time:{build_time}");
 /// ```
-/// 
+///
 pub fn run(template: &str){
     let args :Vec<String> = std::env::args().collect();
-    
+
     if !is_runtime(&args){
         let mut content_template = "
             =============== z1info extended data ====================
@@ -73,8 +73,10 @@ pub fn run(template: &str){
         content = str::replace(&content[..],"{git_info}",&get_commit_id());
         content = str::replace(&content[..],"{build_time}",&format!("{}",get_current_timestamp()));
 
-        let mut file = std::fs::File::create("z1info_tmp").unwrap();
-        file.write_all(content.as_bytes()).expect("write z1info failed");
+        // let mut file = std::fs::File::create("z1info_tmp").unwrap();
+        // file.write_all(content.as_bytes()).expect("write z1info failed");
+
+        write_to_tmp(&content,)
     } else {
         println!("{}",include_str!("../z1info_tmp"));
     }
@@ -132,4 +134,48 @@ fn get_current_timestamp()->u64{
         .expect("Time went backwards");
 
     return since_the_epoch.as_secs();
+}
+
+fn path_exist(path: &str) -> bool {
+    return match std::path::Path::new(path).canonicalize() {
+        Ok(_buf) => true,
+        Err(_error) => false,
+    };
+}
+
+fn write_to_tmp(content: &str) {
+    let cargo_toml = format!("{:?}", std::fs::read_to_string("Cargo.toml"));
+    let mut file = std::fs::File::create("z1info_tmp").unwrap();
+    let mut write_to_crate = true;
+
+    let cargo_toml_info: Vec<&str> = cargo_toml.split("z1info_rust = ").collect();
+    if cargo_toml_info.len()>1{
+        let cargo_toml_info2: Vec<&str> = cargo_toml_info[1].split("\\\"").collect();
+
+        if cargo_toml_info2.len()>1{
+            let cargo_toml_info2: Vec<&str> = cargo_toml_info[1].split("\\\"").collect();
+            let mut cargo_registry = String::from("");
+
+            match std::env::var("CARGO_HOME") {
+                Ok(val) => cargo_registry = format!("{}/registry/src", val),
+                Err(_e) => println!("{:?}",_e),
+            }
+
+            let paths = std::fs::read_dir(cargo_registry).unwrap();
+            for path in paths {
+                let tmp_file = &format!("{}/z1info_rust-{}/z1info_tmp", path.unwrap().path().display(),cargo_toml_info2[1])[..];
+
+                if path_exist(tmp_file){
+                    file = std::fs::File::create(tmp_file).unwrap();
+                    file.write_all(content.as_bytes()).expect("write z1info failed");
+
+                    write_to_crate = false;
+                }
+            }
+        }
+    }
+
+    if write_to_crate{
+        file.write_all(content.as_bytes()).expect("write z1info failed");
+    }
 }
